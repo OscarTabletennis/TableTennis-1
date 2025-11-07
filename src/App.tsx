61517ced9317594f52840317effaa28c0f8d23b5
OscarTabletennis <oscar.liu.iphone@gmail.com>
2025-11-07 18:48:08 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
 import React from 'react'
-export default function App(){
-  return <div className="p-8 text-center">專案已就緒！請把你先前的 App.tsx 貼回來或沿用舊版 App.tsx。</div>
+
+export default function App() {
+  return (
+    <div className="p-8 text-center">
+      專案已就緒！請把你先前的內容再貼回來或改在這個 App.tsx 上。
+    </div>
+  )
+}
+
